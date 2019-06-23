fe37e8ab485fceae99539537c863c5b547946527
dotnet31102018 <44984976+dotnet31102018@users.noreply.github.com>
2019-06-23 19:37:00 +0300

    Add files via upload

---
+++ delegate_async.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _2306_
+{
+    class Program
+    {
+
+       
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            Func<int> func = new Func<int>(() => {
+                int number = random.Next(1000);
+                Thread.Sleep(number);
+                Console.WriteLine(number);
+                return number;
+            });
+
+            object o1 = func.DynamicInvoke(); // blocking
+            Console.WriteLine(o1);
+
+            IAsyncResult async = func.BeginInvoke((IAsyncResult ar) =>
+            {
+                Console.WriteLine("after..........");
+
+                Console.WriteLine(ar.AsyncState); // this is the "hello" parameter
+
+                int res = func.EndInvoke(ar);
+                Console.WriteLine("what was the result? " + res);
+
+            }, "hello");
+
+            int result = func.EndInvoke(async);
+            Console.WriteLine("what was the result? " + result);
+        }
+    }
+}
